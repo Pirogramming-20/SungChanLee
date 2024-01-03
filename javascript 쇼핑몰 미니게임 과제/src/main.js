f8822f23d2a96957ac2a1edbf35aec6c4536a996
Leeseungchan <wkdvnddl050701@gmail.com>
2024-01-04 00:47:08 +0900

    JavaScript - handling events

--- javascript 쇼핑몰 미니게임 과제/src/main.js
+++ javascript 쇼핑몰 미니게임 과제/src/main.js
@@ -19,6 +19,27 @@ function createHTMLString(item) {
           <span class="item__description">${item.gender}, ${item.size}</span>
       </li>`;
 }
+
+
+function onButtonClick(event, items) {
+    const dataset = event.target.dataset;
+    const key = dataset.key;
+    const value = dataset.value;
+  
+    if (key == null || value == null) {
+      return;
+    }
+  
+    displayItems(items.filter(item => item[key] === value));
+}
+  
+function setEventListeners(items) {
+    const logo = document.querySelector('.logo');
+    const buttons = document.querySelector('.buttons');
+    logo.addEventListener('click', () => displayItems(items));
+    buttons.addEventListener('click', event => onButtonClick(event, items));
+}
+  
   
 //main
 loadItems()
