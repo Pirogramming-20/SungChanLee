f6e1b9232e2b8c3cedf2902fb0555b1f52858e5c
Leeseungchan <wkdvnddl050701@gmail.com>
2024-01-06 00:15:37 +0900

    파이썬 1번 배스킨라빈스31 게임 6단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,17 +1,40 @@
 num = 0
 while(True):
-    num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-    try:
-        num_1to3 = int(num_1to3)
-        if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
-            raise Exception("1,2,3 중 하나를 입력하세요")
-    except ValueError:
-        print("정수를 입력하세요")
-    except Exception as e:
-        print(e)
-    else:
-        break
-num = 3
-for i in range(num, num+num_1to3):
-    print("playerA : ",i + 1)
-num += num_1to3
+    # playerA
+    while(True):
+        num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        try:
+            num_1to3 = int(num_1to3)
+            if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
+                raise Exception("1,2,3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
+        except Exception as e:
+            print(e)
+        else:
+            break
+    for i in range(num + 1, num+num_1to3 + 1):
+        print("playerA :",i)
+        if i == 31:
+            print("playerA win!")
+            break
+    num += num_1to3
+    # playerB
+    while(True):
+        num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        try:
+            num_1to3 = int(num_1to3)
+            if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
+                raise Exception("1,2,3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
+        except Exception as e:
+            print(e)
+        else:
+            break
+    for i in range(num + 1 , num+num_1to3 + 1):
+        print("playerB :",i )
+        if i == 31:
+            print("playerB win!")
+            break
+    num += num_1to3
