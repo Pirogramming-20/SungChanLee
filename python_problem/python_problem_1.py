b22e3cd7a85cc87a65c199a91ad86c06e089acb3
Leeseungchan <wkdvnddl050701@gmail.com>
2024-01-06 00:29:13 +0900

    파이썬 1번 배스킨라빈스31 게임 8단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,42 +1,32 @@
 num = 0
+def brGame():
+    while(True):
+        num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+        try:
+            num_1to3 = int(num_1to3)
+            if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
+                raise Exception("1,2,3 중 하나를 입력하세요")
+        except ValueError:
+            print("정수를 입력하세요")
+        except Exception as e:
+            print(e)
+        else:
+            return num_1to3 
+        
 while(True):
     # playerA
-    while(True):
-        num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-        try:
-            num_1to3 = int(num_1to3)
-            if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
-                raise Exception("1,2,3 중 하나를 입력하세요")
-        except ValueError:
-            print("정수를 입력하세요")
-        except Exception as e:
-            print(e)
-        else:
-            break
-    for i in range(num + 1, num+num_1to3 + 1):
+    num_input = brGame()
+    for i in range(num + 1, num + num_input + 1):
         print("playerA :",i)
         if i == 31:
-            # 6단계에서 해버렸어요..... 근데 승자가 반대라 수정할게요
             print("playerB win!")
             break
-    num += num_1to3
+    num += num_input
     # playerB
-    while(True):
-        num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
-        try:
-            num_1to3 = int(num_1to3)
-            if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
-                raise Exception("1,2,3 중 하나를 입력하세요")
-        except ValueError:
-            print("정수를 입력하세요")
-        except Exception as e:
-            print(e)
-        else:
-            break
-    for i in range(num + 1 , num+num_1to3 + 1):
+    num_input = brGame()
+    for i in range(num + 1 , num + num_input + 1):
         print("playerB :",i )
         if i == 31:
-            # 6단계에서 해버렸어요..... 근데 승자가 반대라 수정할게요
             print("playerA win!")
             break
-    num += num_1to3
+    num += num_input
