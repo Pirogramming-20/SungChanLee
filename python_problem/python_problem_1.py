f25cd7955e6626c7d2eed0f9331305fc3b49b80d
Leeseungchan <wkdvnddl050701@gmail.com>
2024-01-05 23:59:54 +0900

    파이썬 1번 배스킨라빈스31 게임 3단계

--- python_problem/python_problem_1.py
+++ python_problem/python_problem_1.py
@@ -1,2 +1,13 @@
 num = 0
-num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+while(True):
+    num_1to3 = input("부를 숫자의 개수를 입력하세요(1, 2, 3만 입력 가능) : ")
+    try:
+        num_1to3 = int(num_1to3)
+        if num_1to3 != 1 and num_1to3 != 2 and num_1to3 != 3:
+            raise Exception("1,2,3 중 하나를 입력하세요")
+    except ValueError:
+        print("정수를 입력하세요")
+    except Exception as e:
+        print(e)
+    else:
+        break
